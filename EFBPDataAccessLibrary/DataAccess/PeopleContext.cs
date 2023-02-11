6a68a880d44959225235fd556106bd3bdabb5a32
Mamdouh Douidar <mamdouidar@gmail.com>
2023-02-11 20:32:52 +0200

    30:00

---
+++ EFBPDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,20 @@
+﻿using EFBPDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFBPDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
